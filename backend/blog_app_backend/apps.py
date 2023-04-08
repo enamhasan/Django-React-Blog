1c21f2779335198e1e0c69c73deddd4e5d6bf5bd
enamhasa <enamhasan@gmail.com>
2023-04-08 18:59:21 -0400

    First commit. Added post model

---
+++ backend/blog_app_backend/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class BlogAppBackendConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'blog_app_backend'
---
+++ backend/blog_app_backend/models.py
@@ -0,0 +1,65 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+from django.db.models.signals import post_save, pre_save
+from django.dispatch import receiver
+from django.utils import timezone
+
+from .utils import unique_slug_generator
+
+User = get_user_model()
+
+class Post(models.Model):
+    """Model For Blog Posts"""
+
+    title = models.CharField(max_length=100)
+    body = models.TextField()
+    thumbnail = models.ImageField(upload_to='post_thumbnail', blank=True, null=True)
+    short_description = models.TextField(max_length=255)
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='posts', related_query_name='post')
+    slug = models.SlugField(blank=True, null=True)
+    is_published = models.BooleanField(default=False)
+    created_on = models.DateTimeField(auto_now_add=True)
+    published_on = models.DateTimeField(null=True, blank=True)
+    last_edited = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+    @property
+    def comments_list(self):
+        return self.comments.filter(is_displayed=True)
+
+    @property
+    def total_comments(self):
+       return self.comments_list.count()
+    """@property
+    def comment_count(self):
+        #return Commnets.objects.filter(Post=self).aggregate(Sum('quantity'))['quantity__sum'] """
+    @property
+    def author_full_name(self):
+        try:
+            return f'{self.author.first_name} {self.author.last_name}'
+        except:
+            return "Name Not Set"
+
+    class Meta:
+        indexes = [models.Index(fields=['slug'])]
+        ordering = ['-published_on']
+
+
+@receiver(post_save, sender=Post)
+def generate_unique_slug_for_posts(sender, instance, created, *args, **kwargs):
+    if created:
+        instance.slug = unique_slug_generator(instance)
+        instance.save()
+
+
+@receiver(pre_save, sender=Post)
+def update_published_on(sender, instance, **kwargs):
+    """Update The Date Of 'Published On' When The Post Gets Published"""
+
+    if instance.id:
+        old_value = Post.objects.get(pk=instance.id).published_on
+        if not old_value:
+            instance.published_on = timezone.now()
