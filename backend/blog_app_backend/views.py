60a06752117a66d2c1e0cfaa21458d58cc7f8130
enamhasa <enamhasan@gmail.com>
2023-06-24 15:17:06 -0400

    make Contact component functional

--- backend/blog_app_backend/views.py
+++ backend/blog_app_backend/views.py
@@ -49,18 +49,16 @@ class RecentPostsView(ListAPIView):
 
 
 class ContactView(APIView):
-    serializer_class = ContactSerializer
     permission_classes = (permissions.AllowAny,)
 
     def post(self, request, format=None):
-        data = self.request.data
-        print(data)
-        serializer = ContactSerializer(data=data)
+        serializer = ContactSerializer(data=request.data)
+        print(serializer)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
-        print(serializer.errors)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
 class CategoryView(APIView):
 
--- frontend/src/components/Contact.js
+++ frontend/src/components/Contact.js
@@ -1,12 +1,19 @@
 import React, { useState } from 'react';
-import { Form, Button } from 'react-bootstrap';
+import { Form, Button, Alert } from 'react-bootstrap';
+import axios from 'axios';
+
+
 
 const Contact = () => {
   const [formData, setFormData] = useState({
-    name: '',
-    email: '',
+    senderName: '',
+    senderEmail: '',
+    subject: '',
     message: '',
   });
+
+ const [successMessage, setSuccessMessage] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
 
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
@@ -14,57 +21,99 @@ const Contact = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    // Add your logic to submit the form data to the backend
-    console.log(formData);
-    // Reset the form fields
-    setFormData({ name: '', email: '', message: '' });
-  };
+    axios
+      .post(`${process.env.REACT_APP_API_URL}/blog/contact/`, formData)
+    .then((response) => {
+      // Handle the successful response
+      console.log(response.data);
+      // Reset the form fields
+      setFormData({ senderName: '', senderEmail: '', subject: '', message: '' });
+      setSuccessMessage('Your message has been sent successfully!');
+      setErrorMessage('');
+    })
+    .catch((error) => {
+     setErrorMessage('Oops! Something went wrong. Please try again.');
+      setSuccessMessage('');
+      console.error(error);
+    });
+};
 
   return (
     <div className="container">
+    <div className="contact-form-wrapper mt-5 mb-5">
+    {successMessage && <Alert variant="success">{successMessage}</Alert>}
+      {errorMessage && <Alert variant="danger">{errorMessage}</Alert>}
 
-    <Form onSubmit={handleSubmit}>
-      <Form.Group controlId="name">
-        <Form.Label>Name</Form.Label>
-        <Form.Control
-          type="text"
-          name="name"
-          value={formData.name}
-          onChange={handleChange}
-          placeholder="Enter your name"
-          required
-        />
-      </Form.Group>
 
-      <Form.Group controlId="email">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control
-          type="email"
-          name="email"
-          value={formData.email}
-          onChange={handleChange}
-          placeholder="Enter your email"
-          required
-        />
-      </Form.Group>
+        <div className="col-sm-12">
+                <h2>Welcome!</h2>
+                <p className="mb-3"> I'm delighted to hear from you. Please use the form below to get in touch.
+                I'll get back to you promptly. Thank you for reaching out!"
+                </p>
+        </div>
 
-      <Form.Group controlId="message">
-        <Form.Label>Message</Form.Label>
-        <Form.Control
-          as="textarea"
-          name="message"
-          value={formData.message}
-          onChange={handleChange}
-          placeholder="Enter your message"
-          required
-        />
-      </Form.Group>
 
-      <Button variant="primary" type="submit">
-        Submit
-      </Button>
-    </Form>
+      <Form onSubmit={handleSubmit}>
+        <Form.Group controlId="senderName" className="mb-3">
+          <div className="col-sm-6">
+              <Form.Control
+                type="text"
+                name="senderName"
+                value={formData.senderName}
+                onChange={handleChange}
+                placeholder="Enter your name"
+                required
+              />
+             </div>
+        </Form.Group>
 
+        <Form.Group controlId="senderEmail" className="mb-3">
+            <div className="col-sm-6">
+              <Form.Control
+                type="email"
+                name="senderEmail"
+                value={formData.senderEmail}
+                onChange={handleChange}
+                placeholder="Enter your email"
+                required
+              />
+            </div>
+        </Form.Group>
+
+        <Form.Group controlId="subject" className="mb-3">
+        <div className="col-sm-12">
+
+          <Form.Control
+            type="text"
+            name="subject"
+            value={formData.subject}
+            onChange={handleChange}
+            placeholder="Enter the subject"
+          />
+         </div>
+        </Form.Group>
+
+        <Form.Group controlId="message" className="mb-3">
+            <div className="col-sm-12">
+              <Form.Control
+                as="textarea"
+                name="message"
+                value={formData.message}
+                onChange={handleChange}
+                placeholder="Enter your message"
+                rows={6}
+                required
+              />
+            </div>
+        </Form.Group>
+
+        <div className="col-sm-12 d-flex justify-content-end">
+        <Button variant="primary" type="submit" className="mb-3">
+          Submit
+        </Button>
+        </div>
+      </Form>
+    </div>
     </div>
   );
 };
